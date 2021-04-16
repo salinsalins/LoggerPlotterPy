e5b7a44fcae3c599c3d6b77047f9511f8c881168
salinsalins <sanin_andrey@mail.ru>
2021-04-16 13:15:03 +0700

    commit

--- pyqtgraphtest.py
+++ pyqtgraphtest.py
@@ -27,6 +27,7 @@ import numpy as np
 x = np.random.normal(size=1000)
 y = np.random.normal(size=1000)
 plot.plot(x, y, pen=None, symbol='o')  ## setting pen=None disables line drawing
+plot.plot(x, y*2)  ## setting pen=None disables line drawing
 
 plot.setMinimumHeight(300)
 plot.setMinimumWidth(500)
