c5a3623234879016e31e1ba1ab8e239c3b9264f8
salinsalins <sanin_andrey@mail.ru>
2021-12-24 10:58:06 +0700

    commit

--- test.py
+++ test.py
@@ -8,6 +8,7 @@ from PyQt5.QtCore import QPoint
 from PyQt5.Qt import QPen, QFont, Qt, QSize
 from PyQt5.QtGui import QColor, QBrush, QLinearGradient, QGradient, QPainter
 
+import numpy as np
 
 class MyChart(QMainWindow):
     def __init__(self):
@@ -33,45 +34,43 @@ class MyChart(QMainWindow):
             QPoint(28, 25)
         ]
 
-        for i in range(1000000):
-            series.append(QPoint(i, 2*i))
+        N = 1000000
+        data = np.random.random(N)
+        for i in range(N):
+            series.append(i, data[i])
 
         # creating chart object
         chart = QChart()
         chart.legend().hide()
         chart.addSeries(series)
 
-
-        pen = QPen(QColor(200, 200, 200))
-        pen.setWidth(5)
+        pen = QPen(QColor(1, 1, 1))
+        pen.setWidth(1)
         series.setPen(pen)
-
 
         font = QFont('Open Sans')
         font.setPixelSize(40)
         font.setBold(True)
         chart.setTitleFont(font)
-        chart.setTitleBrush(QBrush(Qt.yellow))
+        chart.setTitleBrush(QBrush(Qt.black))
         chart.setTitle('Custom Chart Demo')
 
+        # backgroundGradient = QLinearGradient()
+        # backgroundGradient.setStart(QPoint(0, 0))
+        # backgroundGradient.setFinalStop(QPoint(0, 1))
+        # backgroundGradient.setColorAt(0.0, QColor(175, 201, 182))
+        # backgroundGradient.setColorAt(1.0, QColor(51, 105, 66))
+        # backgroundGradient.setCoordinateMode(QGradient.ObjectBoundingMode)
+        # chart.setBackgroundBrush(backgroundGradient)
 
-        backgroundGradient = QLinearGradient()
-        backgroundGradient.setStart(QPoint(0, 0))
-        backgroundGradient.setFinalStop(QPoint(0, 1))
-        backgroundGradient.setColorAt(0.0, QColor(175, 201, 182))
-        backgroundGradient.setColorAt(1.0, QColor(51, 105, 66))
-        backgroundGradient.setCoordinateMode(QGradient.ObjectBoundingMode)
-        chart.setBackgroundBrush(backgroundGradient)
-
-
-        plotAreaGraident = QLinearGradient()
-        plotAreaGraident.setStart(QPoint(0, 1))
-        plotAreaGraident.setFinalStop(QPoint(1, 0))
-        plotAreaGraident.setColorAt(0.0, QColor(222, 222, 222))
-        plotAreaGraident.setColorAt(1.0, QColor(51, 105, 66))
-        plotAreaGraident.setCoordinateMode(QGradient.ObjectBoundingMode)
-        chart.setPlotAreaBackgroundBrush(plotAreaGraident)
-        chart.setPlotAreaBackgroundVisible(True)
+        # plotAreaGraident = QLinearGradient()
+        # plotAreaGraident.setStart(QPoint(0, 1))
+        # plotAreaGraident.setFinalStop(QPoint(1, 0))
+        # plotAreaGraident.setColorAt(0.0, QColor(222, 222, 222))
+        # plotAreaGraident.setColorAt(1.0, QColor(51, 105, 66))
+        # plotAreaGraident.setCoordinateMode(QGradient.ObjectBoundingMode)
+        # chart.setPlotAreaBackgroundBrush(plotAreaGraident)
+        # chart.setPlotAreaBackgroundVisible(True)
 
         # customize axis
         axisX = QCategoryAxis()
@@ -83,7 +82,7 @@ class MyChart(QMainWindow):
         axisX.setLabelsFont(labelFont)
         axisY.setLabelsFont(labelFont)
 
-        axisPen = QPen(Qt.white)
+        axisPen = QPen(Qt.black)
         axisPen.setWidth(2)
 
         axisX.setLinePen(axisPen)
@@ -93,15 +92,15 @@ class MyChart(QMainWindow):
         axisX.setLabelsBrush(axixBrush)
         axisY.setLabelsBrush(axixBrush)
 
-        axisX.setRange(0, 30)
-        axisX.append('low', 10)
-        axisX.append('medium', 20)
-        axisX.append('high', 30)
+        axisX.setRange(0, 300)
+        # axisX.append('low', 10)
+        # axisX.append('medium', 20)
+        # axisX.append('high', 30)
 
-        axisY.setRange(0, 30)
-        axisY.append('slow', 10)
-        axisY.append('average', 20)
-        axisY.append('fast', 30)
+        # axisY.setRange(0, 30)
+        # axisY.append('slow', 10)
+        # axisY.append('average', 20)
+        # axisY.append('fast', 30)
 
         axisX.setGridLineVisible(False)
         axisY.setGridLineVisible(False)
