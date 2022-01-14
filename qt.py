9c562bce8cd92f47e1bbf62208cefa64cc9e4f2e
salinsalins <sanin_andrey@mail.ru>
2022-01-14 17:12:37 +0700

    commit

---
+++ qt.py
@@ -0,0 +1,37 @@
+import sys
+
+try:
+    import PyQt6
+    import PyQt5
+    import PySide6
+except:
+    pass
+
+if 'PyQt6' in sys.modules:
+    # PyQt6
+    from PyQt6 import QtGui, QtWidgets, QtCore
+    from PyQt6.QtCore import pyqtSignal as Signal, pyqtSlot as Slot
+elif 'PyQt5' in sys.modules:
+    # PyQt5
+    from PyQt5 import QtGui, QtWidgets, QtCore
+    from PyQt5.QtCore import pyqtSignal as Signal, pyqtSlot as Slot
+else:
+    # PySide6
+    from PySide6 import QtGui, QtWidgets, QtCore
+    from PySide6.QtCore import Signal, Slot
+
+
+def _enum(obj, name):
+    parent, child = name.split('.')
+    result = getattr(obj, child, False)
+    if result:  # Found using short name only.
+        return result
+    obj = getattr(obj, parent)  # Get parent, then child.
+    return getattr(obj, child)
+
+
+def _exec(obj):
+    if hasattr(obj, 'exec'):
+        return obj.exec()
+    else:
+        return obj.exec_()
