83a58a616659eda0a9e9282e2ca0158b44fa111a
salinsalins <sanin_andrey@mail.ru>
2022-11-25 14:45:02 +0700

    commit 10.0

--- LoggerPlotterPy.py
+++ LoggerPlotterPy.py
@@ -1235,8 +1235,8 @@ class MainWindow(QMainWindow):
     def timer_handler(self):
         t = time.strftime('%H:%M:%S')
         self.sb_clock.setText(t)
-        if time.time() > self.sb_log.time:
-            self.sb_log.setText('')
+        # if time.time() > self.sb_log.time:
+        #     self.sb_log.setText('')
         # check if in parameters edit mode
         if self.stackedWidget.currentIndex() != 0:
             return
