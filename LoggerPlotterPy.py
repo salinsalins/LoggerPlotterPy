6e247630c3e18aa0e57480f847db7a3ac35b44d7
salinsalins <sanin_andrey@mail.ru>
2022-12-12 17:15:03 +0700

    10.5 NewLogTable -> LogTable

--- LoggerPlotterPy.py
+++ LoggerPlotterPy.py
@@ -567,9 +567,9 @@ class MainWindow(QMainWindow):
                         try:
                             if math.isnan(s.value) and 'mark' in s.marks and 'zero' in s.marks:
                                 mark = s.marks['mark']
-                                mark_value = s.y[mark[0], mark[0] + mark[1]].mean()
+                                mark_value = s.y[mark[0]: mark[0] + mark[1]].mean()
                                 zero = s.marks['mark']
-                                zero_value = s.y[zero[0], zero[0] + zero[1]].mean()
+                                zero_value = s.y[zero[0]: zero[0] + zero[1]].mean()
                                 v = mark_value - zero_value
                                 s.value = v
                         except:
